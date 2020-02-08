ec85bd9e2971f618cbc4ccb28ba5e9f5132fe6db
scottie-schneider <scott@laclave.co>
2020-02-08 16:15:32 -0500

    initialize Firebase, create db.js

--- components/Head.js
+++ components/Head.js
@@ -2,7 +2,7 @@ import Head from "next/head"
 
 export default () => (
   <Head>
-    <title>We are testing head</title>
+    <title>Follow Up Edge</title>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta charSet="utf-8" />
     <meta name="description" content={"test"} />
